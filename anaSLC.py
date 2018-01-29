0f29332ca57b0b71b4fdfce990bbf9ef5c3befd0
Olivier Martineau <omartino@in2p3.fr>
2018-01-29 11:00:50 +0100

    Small modifs on anaTiming

--- anaSLC.py
+++ anaSLC.py
@@ -6,8 +6,10 @@ import math
 import numpy as np
 import pylab as pl
 
+datadir = "/home/martineau/GRAND/GRANDproto35/data/tests/"
+
 def loop(filename):
-   datafile = '../data/'+filename
+   datafile = datadir+filename
    print 'Scanning',datafile
 
    with open(datafile,"r") as f:
@@ -20,56 +22,56 @@ def loop(filename):
    Th = np.zeros(shape=(nevts,3,2))
    Temp = np.zeros(shape=(np.size(evts)))
    maxCoarse = np.zeros(shape=(np.size(evts)))
-   
+
    TrigRate = np.zeros(shape=(nevts,7))
    j = 0
    for i in range(j,nevts):
       evt = evts[i]
       if len(evt)>60:  #59 is ACK size
            #print 'SLC word'
-           
+
            evtsplit = evt.split('\n')
            date.append(evtsplit[1])
            IP = evtsplit[2].split(':')[1]
            board[j] = int(IP[-2:]);
-           
+
 	   for k in range(0,6):
-             VPower[j,k]=evtsplit[k+3].split(':')[1]  
-           
+             VPower[j,k]=evtsplit[k+3].split(':')[1]
+
 	   for k in range(0,3):
              thsplit = evtsplit[k+9].split(':')
              Th[j,k,0] = thsplit[1].split(' ')[0]
              Th[j,k,1] = thsplit[1].split(' ')[1]
-	     
+
            Temp[j] = evtsplit[12].split(':')[1]
 
            for k in range(0,7):
              TrigRate[j,k] = evtsplit[13+k].split(':')[1]
 
 	   maxCoarse[j] = evtsplit[20].split(':')[1]
-	   
+
            j = j+1
-	      
+
    hraw = [hex(int(a)) for a in Temp]  # Transfer back to hexadecimal
-   braw = [bin(int(a)) for a in Temp] 
+   braw = [bin(int(a)) for a in Temp]
    draw = [twos_comp(int(a,16), 13) for a in hraw] #2s complements
-   
-   lab = ['Total','Ch1+','Ch2+','Ch3+','Ch1-','Ch2-','Ch3-']   
+
+   lab = ['Total','Ch1+','Ch2+','Ch3+','Ch1-','Ch2-','Ch3-']
    boards = set(board[np.where(board>0)])
    print 'Boards in run:',list(boards)
    print 'Run start:', date[0]
    for id in boards:
-   	   sel = np.where(board == id)  
+   	   sel = np.where(board == id)
 	   date_end = date[sel[0][-1]]
    	   print 'Run stop:',date_end,'for board',id,' (',np.size(sel),'measurements)'
-	     
+
 	   pl.figure(1)
    	   pl.plot(Temp[sel])
 	   pl.grid(True)
 	   pl.xlabel('SLC measurment nb')
 	   pl.ylabel('Temperature')
 	   pl.title('Temperature')
-	   
+
 	   pl.figure(2)
 	   for i in range(np.shape(VPower)[1]):
 	        sub=321+i
@@ -79,39 +81,40 @@ def loop(filename):
 		if i>3:
   		  pl.xlabel('SLC measurment nb')
 		pl.title('Voltage'+str(i+1))
-		
+
 	   pl.figure(3)  #Trig Rate
 	   for i in range(np.shape(TrigRate)[1]):
 	        if len(boards)>1:  # Subplot if 2 boards
 			sub=211+int(id==list(boards)[-1])
 			pl.subplot(sub)
 		if np.sum(TrigRate[sel,i][0])>0:
-	          pl.plot(TrigRate[sel,i][0],label=lab[i]) 
+	          pl.plot(TrigRate[sel,i][0],label=lab[i])
 		#pl.yscale('log')
 		pl.grid(True)
 		pl.ylabel('TrigRate [Hz]')
 		if (len(boards)<2) | (sub==212 ):
-		        pl.xlabel('SLC measurment nb')   
+		        pl.xlabel('SLC measurment nb')
    		pl.legend()
 		pl.title('Board '+str(id))
-		
+
 	   pl.figure(4)  #MaxCoarse
 	   pl.subplot(211)
 	   pl.plot(maxCoarse[sel])
 	   pl.subplot(212)
 	   pl.hist(maxCoarse[sel])
 	   print 'maxCoarse counter = ',np.mean(maxCoarse[sel]),'+-',np.std(maxCoarse[sel]),' vs 1.25e+8'
-		
+
    pl.draw()
-   
-   return        
- 
+   pl.show()
+
+   return
+
 def twos_comp(val, bits):
    """compute the 2's compliment of int value val"""
    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
      val = val - (1 << bits)	    # compute negative value
-   return val	
-    
+   return val
+
 
 
 if __name__ == '__main__':
--- anaTiming.py
+++ anaTiming.py
@@ -6,8 +6,9 @@ import math
 import numpy as np
 import pylab as pl
 
+datadir = "/home/martineau/GRAND/GRANDproto35/data/tests/"
 def loopEvents(filename):
-   datafile = '../data/'+filename
+   datafile = datadir+filename
    print 'Scanning',datafile
 
    with open(datafile,"r") as f:
@@ -26,7 +27,7 @@ def loopEvents(filename):
    trigtime = np.zeros(shape=(np.size(evts)))
    data = list()
    #maxCoarse = [124997981, 124997993]  # Read from S299_bxx.data
-   maxCoarse = [124998238.5, 124998238.4] # Read from S22_bxx.data
+   maxCoarse = [124997955.469, 124998238.4] # Read from S22_bxx.data
    j = 0
    for i in range(1001,np.size(evts)):  # Skip at least first elemt (empty)
    #for i in range(1001,2001):  # Skip first elemt which is empty
@@ -34,19 +35,19 @@ def loopEvents(filename):
 	   	print 'Event ',i
    	   evt = evts[i]
    	   evtsplit = evt.split('\n')
- 
+
    	   if np.size(evtsplit)>8:   # Event is of normal size
    		   date.append(evtsplit[1])
 		   IP = evtsplit[2][3:]
-		   if IP=='192.168.1.101':  
+		   if IP=='192.168.1.101':
 		   	board[j] = 01;
-		   if IP=='192.168.1.102':  
+		   if IP=='192.168.1.102':
 		   	board[j] = 02;
-				
+
    		   TS2[j]=int(evtsplit[3][4:])  # time elapsed since last PPS (125MHz clock <=> 8ns counter)
    		   tt=int(evtsplit[4][11:])  # phase in 8ns slot fr trigger
    		   TS1Trig[j] = get_1stone(hex(tt))
-		   tpps=int(evtsplit[5][7:]) 
+		   tpps=int(evtsplit[5][7:])
 		   TS1PPS[j]=get_1stone(hex(tpps))  # phase in 8ns slot for PPS
    		   SSS[j]=int(evtsplit[6][4:])  # Elapsed seconds since start
    		   EvtId[j] = int(evtsplit[7][3:])
@@ -56,8 +57,8 @@ def loopEvents(filename):
    		   raw2 = raw[0].split(" ") # Cut raw data list into samples
 		   raw2 = raw2[0:np.size(raw2)-1]   # Remove last element (empty)
 		   hraw2 = [hex(int(a)) for a in raw2]  # TRansfer back to hexadecimal
-   		   draw = [twos_comp(int(a,16), 12) for a in hraw2] #2s complements		   
- 
+   		   draw = [twos_comp(int(a,16), 12) for a in hraw2] #2s complements
+
    		   nsamples = len(draw)/4  # Separate data to each channel
    		   offset = nsamples/2.0
    		   #print nsamples,"samples per channel --> offset = ",offset
@@ -66,8 +67,8 @@ def loopEvents(filename):
    		   print 'Error! Empty event',i
    		   date.append("")
    		   data.append("")
-		   
-	   j = j+1	   
+
+	   j = j+1
 
    # Build trig time
    boards = set(board[np.where(board>0)])
@@ -78,16 +79,16 @@ def loopEvents(filename):
    for id in boards:
      sel = np.where(board == id)
      date_end = date[sel[0][-1]-1]
-     
+
      print 'Run stop:',date_end,'for board',id,' (',np.size(sel),'measurements)'
      pl.figure(12)
      pl.hist(TS2[sel])
      pl.xlabel("Distrib of TS2 (8ns step counter)")
-     
+
      # Ratio of expected nb of counts in 1s to actually measured  => correction to clock
      #cor=1
      #cor=125e6/float(max(TS2[sel]))
-     cor=125e6/float(maxCoarse[int(id)-1]) 
+     cor=125e6/float(maxCoarse[int(id)-1]+1)
      print max(TS2[sel]), maxCoarse[int(id)-1]
      print 'Correction factor for 125MHz clock for board',id,':',cor
      # Build trig time
@@ -95,40 +96,40 @@ def loopEvents(filename):
      if id == 1:
        newS1 = np.where(np.diff(SSS[sel])>0)[[0][-1]]+1  # index of 1st event in new second for board 1
      if id == 2:
-       newS2 = np.where(np.diff(SSS[sel])>0)[[0][-1]]+1  # index of 1st event in new second for board 2  
-          
+       newS2 = np.where(np.diff(SSS[sel])>0)[[0][-1]]+1  # index of 1st event in new second for board 2
+
    trigtime1 = trigtime[np.where(board == list(boards)[0])]
    trigtime2 = trigtime[np.where(board == list(boards)[1])]
    #TS21 = TS2[np.where(board == list(boards)[0])]  # This is the clock count for the 1st event in the new second
    tdiff = (trigtime2-trigtime1)*1e9  # Delta t in ns
    dPPS = (trigtime2[newS2]-trigtime1[newS1])*1e9  # Delta t @ new PPS in ns
-   
+
    tdiff_b1 = np.zeros(shape=(np.size(trigtime1)))
    for i in range(len(trigtime1)-1):
-   	tdiff_b1[i]=(trigtime1[i+1]-trigtime1[i])*1e3  
+   	tdiff_b1[i]=(trigtime1[i+1]-trigtime1[i])*1e3
    tdiff_b2 = np.zeros(shape=(np.size(trigtime2)))
    for i in range(len(trigtime2)-1):
-   	tdiff_b2[i]=(trigtime2[i+1]-trigtime2[i])*1e3  
+   	tdiff_b2[i]=(trigtime2[i+1]-trigtime2[i])*1e3
 
    tdiff_b1 = tdiff_b1[tdiff_b1>0]
    tdiff_b2 = tdiff_b2[tdiff_b2>0]
-   
-   
+
+
    print 'Time delay beween consecutive events for board 1:',np.mean(tdiff_b1),'ms'
    print 'Time delay beween consecutive events for board 2:',np.mean(tdiff_b2),'ms'
-   
-   sel = np.where(np.abs(tdiff)<300)[[0][-1]]  
+
+   sel = np.where(np.abs(tdiff)<300)[[0][-1]]
    print 'Delta Trig Time = ',np.mean(tdiff[sel]),'+-',np.std(tdiff[sel]),'ns'
    print 'Delta Trig Time @ new PPS = ',np.mean(dPPS),'+-',np.std(dPPS),'ns'
    pl.figure(1)
    pl.plot(trigtime1[1:],label='Board 1')
-   pl.hold(True)   
-   pl.grid(True)   
+   pl.hold(True)
+   pl.grid(True)
    pl.plot(trigtime2[1:],'r',label='Board 2')
    pl.xlabel('Event nb')
    pl.ylabel('Trig time [s]')
    pl.legend()
-   
+
    pl.figure(21)
    pl.subplot(311)
    pl.plot(trigtime1[sel],tdiff[sel])
@@ -136,14 +137,14 @@ def loopEvents(filename):
    pl.xlabel('Time [ns]')
    pl.ylabel('trigtime_b2-trigtime_b1 [ns]')
    pl.grid(True)
-   pl.xlim(min(trigtime1),max(trigtime1))     
+   pl.xlim(min(trigtime1),max(trigtime1))
    pl.subplot(312)
-   pl.hist(tdiff[sel],100)   
+   pl.hist(tdiff[sel],100)
    pl.xlabel('trigtime_b2-trigtime_b1 [ns]')
    pl.subplot(313)
-   pl.hist(dPPS,100)   
+   pl.hist(dPPS,100)
    pl.xlabel('trigtime_b2-trigtime_b1 @ newPPS [ns]')
-   
+
    pl.figure(18)
    pl.subplot(221)
    pl.plot(tdiff_b1[1:])
@@ -165,8 +166,8 @@ def loopEvents(filename):
    pl.xlabel('Consecutive Delta Trigtime [ms]')
    pl.show()
 
-   
-   
+
+
 def get_1stone(val):
     if val == '0x1':
     	return 0
@@ -184,16 +185,16 @@ def get_1stone(val):
     	return 6
     if val == '0xff':
     	return 7
-    return 8	
-	
-	
-    
+    return 8
+
+
+
 def twos_comp(val, bits):
     """compute the 2's compliment of int value val"""
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
         val = val - (1 << bits)        # compute negative value
-    return val   
-    
+    return val
+
 
 if __name__ == '__main__':
      loopEvents(sys.argv[1])
